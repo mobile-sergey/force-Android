eab085da70372a8137040b56fce30eadc06e3563
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-18 17:33:02 +0300

    добавлен экран 0.Заставка
    Добавлены модуль SplashScreenActivity, макет splash_screen
    А так же обновлены 5 картинок ic_launcher.png

---
+++ app/src/main/java/club/plus1/forcetaxi/activities/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package club.plus1.forcetaxi.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.content.Intent;
+import android.os.Handler;
+
+import club.plus1.forcetaxi.R;
+
+public class SplashScreenActivity extends Activity {
+
+    // Время в милесекундах, в течение которого будет отображаться Splash Screen
+    private final int SPLASH_DISPLAY_LENGTH = 2000;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splash_screen);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // По истечении времени, запускаем главный активити, а Splash Screen закрываем
+                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
+                SplashScreenActivity.this.startActivity(mainIntent);
+                SplashScreenActivity.this.finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
