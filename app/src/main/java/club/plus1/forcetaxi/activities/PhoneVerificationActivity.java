fcda7b9b2aeb8bc8b7c4e9c9d400844671a52309
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-19 17:51:47 +0300

    добавлен экран 5.Регистрация завершена
    - Добавлен модуль RegistrationFinishedActivity.java
    и макет registration_finished.xml
    - Добавлен переход в этот экран при нажатии кнопки "Подтвердить"
    в экране "4. Регистрация. Подтверждение телефона"
    - Добавлены картинки yes.png, no.png, wait.png для отображения статуса

--- app/src/main/java/club/plus1/forcetaxi/activities/PhoneVerificationActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/activities/PhoneVerificationActivity.java
@@ -2,15 +2,30 @@ package club.plus1.forcetaxi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import club.plus1.forcetaxi.R;
 
 public class PhoneVerificationActivity extends AppCompatActivity {
 
+    Button butonVerification;
+    View.OnClickListener listenerVerification = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(PhoneVerificationActivity.this, RegistrationFinishedActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.phone_verification);
+
+        butonVerification = findViewById(R.id.buttonVerification);
+        butonVerification.setOnClickListener(listenerVerification);
     }
 }
