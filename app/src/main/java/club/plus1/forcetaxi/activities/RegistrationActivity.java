c4848cb770cf4adbeae3ba9d7d7e4e941781b361
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-19 16:47:46 +0300

    добавлен экран 4.Регистрация. Подтверждение телефона
    Добавлен модуль PhoneVerificationActivity.java и макет phone_verification.xml
    Также добавлен переход в этот экран при нажатии кнопки "Регистрация"
    в экране "3. Регистрация"

--- app/src/main/java/club/plus1/forcetaxi/activities/RegistrationActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/activities/RegistrationActivity.java
@@ -2,15 +2,31 @@ package club.plus1.forcetaxi.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import club.plus1.forcetaxi.R;
 
 public class RegistrationActivity extends AppCompatActivity {
 
+    Button butonRegistration;
+    View.OnClickListener listenerRegistraiton = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(RegistrationActivity.this, PhoneVerificationActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration);
+
+        butonRegistration = findViewById(R.id.buttonEnter);
+        butonRegistration.setOnClickListener(listenerRegistraiton);
+
     }
 }
