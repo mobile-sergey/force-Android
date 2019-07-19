f26eb8ad991901b6d776b2226e93f2459555c5c1
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-19 10:07:04 +0300

    добавлен экран 1.Вход
    Переименован модуль MainActivity.java в EnterActivity.java
    Переименован модуль main_activity.xml в enter.xml
    Переименован модуль SplashScreenActivity.java в SplashActivity.java
    Переименован макет splash_screen.xml в splash.xml

--- app/src/main/java/club/plus1/forcetaxi/activities/EnterActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/activities/EnterActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 
 import club.plus1.forcetaxi.R;
 
-public class MainActivity extends AppCompatActivity {
+public class EnterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.enter);
     }
 }
--- app/src/main/java/club/plus1/forcetaxi/activities/SplashActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/activities/SplashActivity.java
@@ -7,7 +7,7 @@ import android.os.Handler;
 
 import club.plus1.forcetaxi.R;
 
-public class SplashScreenActivity extends Activity {
+public class SplashActivity extends Activity {
 
     // Время в милесекундах, в течение которого будет отображаться Splash Screen
     private final int SPLASH_DISPLAY_LENGTH = 2000;
@@ -15,15 +15,15 @@ public class SplashScreenActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.splash_screen);
+        setContentView(R.layout.splash);
 
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 // По истечении времени, запускаем главный активити, а Splash Screen закрываем
-                Intent mainIntent = new Intent(SplashScreenActivity.this, MainActivity.class);
-                SplashScreenActivity.this.startActivity(mainIntent);
-                SplashScreenActivity.this.finish();
+                Intent mainIntent = new Intent(SplashActivity.this, EnterActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
     }
