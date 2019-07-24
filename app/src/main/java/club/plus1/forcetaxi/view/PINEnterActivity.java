925756d80e48f7c39469755bf225a50df12b5282
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-24 15:03:50 +0300

    добавлен экран "33.Ввод ПИН"
    - Добавлен модуль PINEnterActivity.java
    - Добавлен макет pin_enter.xml
    - Теперь при нажатии на экране "32.Установка ПИН. Результат"
    переходит на экран "33.Ввод ПИН"
    - Теперь при нажатии на кнопку "Продолжить" на экране "33.Ввод ПИН"
    переходит на экран "1.Вход"

--- app/src/main/java/club/plus1/forcetaxi/view/PINEnterActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/view/PINEnterActivity.java
@@ -5,17 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 import club.plus1.forcetaxi.R;
 
-public class PINResultActivity extends AppCompatActivity {
+public class PINEnterActivity extends AppCompatActivity {
 
-    View layout;
-
-    View.OnClickListener listenerLayout = new View.OnClickListener() {
+    Button butonNext;
+    View.OnClickListener listenerNext = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Intent intent = new Intent(PINResultActivity.this, PINEnterActivity.class);
+            Intent intent = new Intent(PINEnterActivity.this, EnterActivity.class);
             startActivity(intent);
         }
     };
@@ -23,9 +23,9 @@ public class PINResultActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.pin_result);
+        setContentView(R.layout.pin_enter);
 
-        layout = findViewById(R.id.layout);
-        layout.setOnClickListener(listenerLayout);
+        butonNext = findViewById(R.id.buttonNext);
+        butonNext.setOnClickListener(listenerNext);
     }
 }
