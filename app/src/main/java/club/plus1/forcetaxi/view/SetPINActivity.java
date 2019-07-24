720e01d2b643af1f412918cd58e5b78c82ad403c
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-24 14:29:50 +0300

    добавлен экран "31.Подтверждение ПИН"
    - Добавлен модуль ConfirmPINActivity.java
    - Добавлен макет confirm_pin.xml
    - Теперь при нажатии кнопки "Продолжить" на экране
    "30.Установка ПИН" переходит на "31.Подтверждение ПИН"

--- app/src/main/java/club/plus1/forcetaxi/view/SetPINActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/view/SetPINActivity.java
@@ -2,15 +2,30 @@ package club.plus1.forcetaxi.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import club.plus1.forcetaxi.R;
 
 public class SetPINActivity extends AppCompatActivity {
 
+    Button butonNext;
+    View.OnClickListener listenerNext = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(SetPINActivity.this, ConfirmPINActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.set_pin);
+
+        butonNext = findViewById(R.id.buttonNext);
+        butonNext.setOnClickListener(listenerNext);
     }
 }
