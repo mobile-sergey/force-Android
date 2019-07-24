6153f218bf8faf31dbd78b1ddaaaec34a10af495
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-24 14:21:24 +0300

    добавлен экран "30.Установка ПИН"
    - Добавлен модуль SetPINActivity.java
    - Добавлен макет set_pin.xml
    - Теперь при нажатии ссылки "Установить пин" на экране
    "5.Регистрация завершена" переходит на "30.Установка ПИН"

--- app/src/main/java/club/plus1/forcetaxi/view/RegistrationFinishedActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/view/RegistrationFinishedActivity.java
@@ -2,15 +2,30 @@ package club.plus1.forcetaxi.view;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import club.plus1.forcetaxi.R;
 
 public class RegistrationFinishedActivity extends AppCompatActivity {
 
+    TextView buttonSetPIN;
+    View.OnClickListener listenerSetPIN = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(RegistrationFinishedActivity.this, SetPINActivity.class);
+            startActivity(intent);
+        }
+    };
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.registration_finished);
+
+        buttonSetPIN = findViewById(R.id.linkSetPIN);
+        buttonSetPIN.setOnClickListener(listenerSetPIN);
     }
 }
---
+++ app/src/main/java/club/plus1/forcetaxi/view/SetPINActivity.java
@@ -0,0 +1,16 @@
+package club.plus1.forcetaxi.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import club.plus1.forcetaxi.R;
+
+public class SetPINActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.set_pin);
+    }
+}
