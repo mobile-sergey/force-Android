1f11f1b74b54de078eb5067d0e03acc6fc3f9c16
lavsexpert <lavrov-sergey@yandex.ru>
2019-07-26 00:10:58 +0300

    рефакторинг модулей SplashAcitvity.java и SplashViewModel.java
    - Асинхронный запуск экрана "1.Вход" перенесён в SplashViewModel.java
    - Теперь в SplashActivity.java только стандартные вызовы

--- app/src/main/java/club/plus1/forcetaxi/view/SplashActivity.java
+++ app/src/main/java/club/plus1/forcetaxi/view/SplashActivity.java
@@ -1,9 +1,7 @@
 package club.plus1.forcetaxi.view;
 
 import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
-import android.os.Handler;
 
 import androidx.databinding.DataBindingUtil;
 
@@ -13,30 +11,15 @@ import club.plus1.forcetaxi.viewmodel.SplashViewModel;
 
 public class SplashActivity extends Activity {
 
-    // Время в милесекундах, в течение которого будет отображаться Splash Screen
-    private final int SPLASH_DISPLAY_LENGTH = 1000;
-
     private SplashViewModel viewModel;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        // Создаём ViewModel и показываем экран исползуя биндинги
-        viewModel = new SplashViewModel(this);
         SplashBinding binding = DataBindingUtil.setContentView(this, R.layout.splash);
+        viewModel = new SplashViewModel(this);
+        viewModel.startEnterActivity(this);
         binding.setSplash(viewModel);
-
-        // Запуск экрана "1.Вход" параллельно через некоторое время после завершения загрузки приложения
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                // По истечении времени, запускаем главный активити, а Splash Screen закрываем
-                Intent mainIntent = new Intent(SplashActivity.this, EnterActivity.class);
-                SplashActivity.this.startActivity(mainIntent);
-                SplashActivity.this.finish();
-            }
-        }, SPLASH_DISPLAY_LENGTH);
     }
 
     @Override
--- app/src/main/java/club/plus1/forcetaxi/viewmodel/SplashViewModel.java
+++ app/src/main/java/club/plus1/forcetaxi/viewmodel/SplashViewModel.java
@@ -1,12 +1,17 @@
 package club.plus1.forcetaxi.viewmodel;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.pm.PackageInfo;
 import android.content.pm.PackageManager;
+import android.os.Handler;
+
+import club.plus1.forcetaxi.view.EnterActivity;
 
 public class SplashViewModel {
     public String versionName;
     public int versionCode;
+    private final int SPLASH_TIME = 1000;
 
     public SplashViewModel(Context context) {
         setVersion(context);
@@ -16,23 +21,33 @@ public class SplashViewModel {
         setVersion(context);
     }
 
+    // Получаем информацию о пакете из которого получим версию и номер сборки
+    // Если не удалось обратиться к пакету - создаём каркас с необходимыми нам полями
     public void setVersion(Context context) {
-        // Получаем информацию о пакете из которого получим версию и номер сборки
         PackageInfo pInfo = null;
         try {
             pInfo = context.getPackageManager().getPackageInfo(context.getPackageName(), 0);
         } catch (PackageManager.NameNotFoundException e) {
             e.printStackTrace();
         }
-
-        // Если не удалось обратиться к пакету - создаём каркас с необходимыми нам полями
         if (pInfo == null) {
             pInfo = new PackageInfo();
             pInfo.versionName = "0.0";
             pInfo.versionCode = 0;
         }
-
         this.versionName = pInfo.versionName;
         this.versionCode = pInfo.versionCode;
+    }
+
+    // Запуск экрана "1.Вход" параллельно через некоторое время после завершения загрузки приложения
+    public void startEnterActivity(final Context context) {
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // По истечении времени, запускаем главный активити, а Splash Screen закрываем
+                Intent mainIntent = new Intent(context, EnterActivity.class);
+                context.startActivity(mainIntent);
+            }
+        }, SPLASH_TIME);
     }
 }
